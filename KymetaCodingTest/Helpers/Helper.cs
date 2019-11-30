c32c1b257ba09cb77452c3fb92bff2577b5e2ee8
Fred Boundou <boundoufred@gmail.com>
2019-11-29 16:40:09 -0800

    Implementing Main part of the coding test

---
+++ KymetaCodingTest/Helpers/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KymetaCodingTest.Helpers
+{
+    public class Helper
+    {
+        public Helper()
+        {
+
+        }
+        public bool convertStringToNumber(string str, int newVal)
+        {
+            if (str.Length > 0)
+            {
+                if (Int32.TryParse(str, out newVal))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
--- KymetaCodingTest/MainWindow.xaml.cs
+++ KymetaCodingTest/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KymetaCodingTest.Helpers;
+using System;
+using System.Text.RegularExpressions;
+using System.Timers;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KymetaCodingTest
 {
@@ -20,9 +13,100 @@ namespace KymetaCodingTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        Timer timer;
+        public int counter = 0;
+        public int second = 0;
+        public int newVal = 0;
+        Helper helper = new Helper();
+
+        // lock handle for shared result
+        private static object lockHandle = new object();
+
+        // event wait handles
+        private readonly System.Threading.ManualResetEvent setResult = new System.Threading.ManualResetEvent(false);
+
         public MainWindow()
         {
             InitializeComponent();
+            resultTextBlock.Text = counter.ToString();
+
+            System.Threading.Thread task = new System.Threading.Thread(backgroundTask);
+            task.IsBackground = true; // execute the task in background
+            task.Start();
+
+        }
+
+        private void backgroundTask()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Elapsed += onTimeElapsed;
+            timer.Start();
+            setResult.WaitOne();
+        }
+        private void onTimeElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                ((Timer)sender).Stop();
+                second += 1;
+                resultOfNumberOfSecond.Text = string.Format("{0}", second.ToString());
+                ((Timer)sender).Start();
+            });
+        }
+
+        /// <summary>
+        /// Reset the timer at the beginning zero.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resetNumberOfSecond_Click(object sender, RoutedEventArgs e)
+        {
+            second = 0;
+            resultOfNumberOfSecond.Text = second.ToString();
+        }
+
+        /// <summary>
+        /// Set the the timer to new value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void setArbitraryValue_Click(object sender, RoutedEventArgs e)
+        {
+            if (arbitraryValueTextBox.Text.Length > 0)
+            {
+                if (Int32.TryParse(arbitraryValueTextBox.Text, out newVal))
+                {
+                    lock (lockHandle)
+                    {
+                        second = newVal;
+                    }
+                    resultOfNumberOfSecond.Text = second.ToString();
+                    setResult.Set();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Increment the counter by 1 when clicking on the button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void incrementCounter_Click(object sender, RoutedEventArgs e)
+        {
+            lock(lockHandle)
+            {
+                counter++;
+            }
+            resultTextBlock.Text = counter.ToString();
+            setResult.Set();
+        }
+
+        // This method allows to validate the input. Only numeric value is allowed
+        private void validateNumberTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
